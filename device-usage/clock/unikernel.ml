8452a7a92255f276efdd66b0a99652f8150244e7
Hannes Mehnert <hannes@mehnert.org>
2018-02-24 16:07:34 +0100

    .

--- device-usage/clock/unikernel.ml
+++ device-usage/clock/unikernel.ml
@@ -15,7 +15,7 @@ module Main (Time: Mirage_types_lwt.TIME) (PClock: Mirage_types.PCLOCK) (MClock:
       let ns = MClock.elapsed_ns mclock in
       Log.info (fun f -> f "ptime %a %Lu ns elapsed since boot, %Lu ns since last output"
                    (Ptime.pp_human ()) current_time ns (Int64.sub ns old_ns)) ;
-      OS.Time.sleep_ns 1_000_000_000L >>= fun () ->
+      OS.Time.sleep_ns 1_000_000L >>= fun () ->
       speak pclock mclock ns ()
     in
     speak pclock mclock (MClock.elapsed_ns mclock) ()
