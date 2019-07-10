0b39e20e0a854986c12648c5fe8a7aead9b2857d
Hannes Mehnert <hannes@mehnert.org>
2019-07-10 19:08:46 +0200

    use two devices, solo5-hvt --net:service=tap0 --net:management=tap10 -- network.hvt --ipv4=10.0.42.2/24 --management-ipv4=192.168.0.2/24

--- device-usage/network/config.ml
+++ device-usage/network/config.ml
@@ -4,11 +4,13 @@ let port =
   let doc = Key.Arg.info ~doc:"The TCP port on which to listen for incoming connections." ["port"] in
   Key.(create "port" Arg.(opt int 8080 doc))
 
-let main = foreign ~keys:[Key.abstract port] "Unikernel.Main" (stackv4 @-> job)
+let main = foreign ~keys:[Key.abstract port] "Unikernel.Main" (stackv4 @-> stackv4 @-> job)
 
 let stack = generic_stackv4 default_network
 
+let stack2 = generic_stackv4 ~group:"management" (netif ~group:"management" "management")
+
 let () =
   register "network" [
-    main $ stack
+    main $ stack $ stack2
   ]
--- device-usage/network/unikernel.ml
+++ device-usage/network/unikernel.ml
@@ -1,21 +1,33 @@
 open Lwt.Infix
 
-module Main (S: Mirage_stack_lwt.V4) = struct
-
-  let start s =
-    let port = Key_gen.port () in
+module H (S : Mirage_stack_lwt.V4) (L : Logs.LOG) = struct
+  let listen s port =
     S.listen_tcpv4 s ~port (fun flow ->
         let dst, dst_port = S.TCPV4.dst flow in
-        Logs.info (fun f -> f "new tcp connection from IP %s on port %d"
-                  (Ipaddr.V4.to_string dst) dst_port);
+        L.info (fun f -> f "new tcp connection from IP %s on port %d"
+                     (Ipaddr.V4.to_string dst) dst_port);
         S.TCPV4.read flow >>= function
-        | Ok `Eof -> Logs.info (fun f -> f "Closing connection!"); Lwt.return_unit
-        | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
+        | Ok `Eof -> L.info (fun f -> f "Closing connection!"); Lwt.return_unit
+        | Error e -> L.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
         | Ok (`Data b) ->
-          Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.len b) (Cstruct.to_string b));
+          L.info (fun f -> f "read: %d bytes:@.%s" (Cstruct.len b) (Cstruct.to_string b));
           S.TCPV4.close flow
       );
-
     S.listen s
+end
 
+let src = Logs.Src.create "service" ~doc:"Service logging"
+module SLog = (val Logs.src_log src : Logs.LOG)
+
+let src = Logs.Src.create "management" ~doc:"Management logging"
+module MLog = (val Logs.src_log src : Logs.LOG)
+
+module Main (S: Mirage_stack_lwt.V4) (M : Mirage_stack_lwt.V4) = struct
+
+  module Management = H(M)(MLog)
+  module Service = H(S)(SLog)
+
+  let start s m =
+    let port = Key_gen.port () in
+    Lwt.join [ Service.listen s port ; Management.listen m port ]
 end
