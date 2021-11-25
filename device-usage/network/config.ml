e3ad1e1a94476a2c4ed0e105e2cbd25335016b91
Hannes Mehnert <hannes@mehnert.org>
2021-11-25 11:08:20 +0100

    more stacks

--- device-usage/network/config.ml
+++ device-usage/network/config.ml
@@ -14,23 +14,60 @@ let tcpv4_direct_conf () = object
     | _ -> failwith "direct tcpv4"
 end
 
-let direct_tcp
+let direct_tcpv4
     ?(clock=default_monotonic_clock)
     ?(random=default_random)
     ?(time=default_time) ip =
   impl (tcpv4_direct_conf ()) $ random $ clock $ time $ ip
 
+let tcpv6_direct_conf () = object
+  inherit base_configurable
+  method ty = random @-> mclock @-> time @-> ipv6 @-> (tcp: 'a tcp typ)
+  method name = "tcp"
+  method module_name = "Utcp_mirage.Make_v6"
+  method! connect _ modname = function
+    | [_random; _mclock; _time; ip] -> Fmt.str "Lwt.return (%s.connect %s)" modname ip
+    | _ -> failwith "direct tcpv6"
+end
+
+let direct_tcpv6
+    ?(clock=default_monotonic_clock)
+    ?(random=default_random)
+    ?(time=default_time) ip =
+  impl (tcpv6_direct_conf ()) $ random $ clock $ time $ ip
+
+let tcpv4v6_direct_conf () = object
+  inherit base_configurable
+  method ty = random @-> mclock @-> time @-> ipv4v6 @-> (tcp: 'a tcp typ)
+  method name = "tcp"
+  method module_name = "Utcp_mirage.Make_v4v6"
+  method! connect _ modname = function
+    | [_random; _mclock; _time; ip] -> Fmt.str "Lwt.return (%s.connect %s)" modname ip
+    | _ -> failwith "direct tcpv4v6"
+end
+
+let direct_tcpv4v6
+    ?(clock=default_monotonic_clock)
+    ?(random=default_random)
+    ?(time=default_time) ip =
+  impl (tcpv4v6_direct_conf ()) $ random $ clock $ time $ ip
+
 let main =
   foreign ~keys:[Key.abstract port]
     ~packages:[ package ~sublibs:["mirage"] "utcp"]
-    "Unikernel.Main" (stackv4 @-> job)
+    "Unikernel.Main" (stackv4v6 @-> job)
 
 let stack =
   let e = etif default_network in
   let a = arp e in
-  let i = create_ipv4 e a in
-  let tcp = direct_tcp i in
-  direct_stackv4 ~tcp default_network e a i
+  let i4 = create_ipv4 e a in
+  let i6 = create_ipv6 default_network e in
+  let i4i6 = create_ipv4v6 i4 i6 in
+  let tcp = direct_tcpv4v6 i4i6 in
+  let ipv4_only = Key.ipv4_only ()
+  and ipv6_only = Key.ipv6_only ()
+  in
+  direct_stackv4v6 ~tcp ~ipv4_only ~ipv6_only default_network e a i4 i6
 
 let () =
   register "network" [
--- device-usage/network/unikernel.ml
+++ device-usage/network/unikernel.ml
@@ -1,19 +1,19 @@
 open Lwt.Infix
 
-module Main (S: Mirage_stack.V4) = struct
+module Main (S: Mirage_stack.V4V6) = struct
 
   let start s =
     let port = Key_gen.port () in
-    S.listen_tcpv4 s ~port (fun flow ->
-        let dst, dst_port = S.TCPV4.dst flow in
-        Logs.info (fun f -> f "new tcp connection from IP %s on port %d"
-                  (Ipaddr.V4.to_string dst) dst_port);
-        S.TCPV4.read flow >>= function
+    S.listen_tcp s ~port (fun flow ->
+        let dst, dst_port = S.TCP.dst flow in
+        Logs.info (fun f -> f "new tcp connection from IP %a on port %d"
+                      Ipaddr.pp dst dst_port);
+        S.TCP.read flow >>= function
         | Ok `Eof -> Logs.info (fun f -> f "Closing connection!"); Lwt.return_unit
-        | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCPV4.pp_error e); Lwt.return_unit
+        | Error e -> Logs.warn (fun f -> f "Error reading data from established connection: %a" S.TCP.pp_error e); Lwt.return_unit
         | Ok (`Data b) ->
-          Logs.debug (fun f -> f "read: %d bytes:\n%s" (Cstruct.length b) (Cstruct.to_string b));
-          S.TCPV4.close flow
+          Logs.info (fun f -> f "read: %d bytes:\n%s" (Cstruct.length b) (Cstruct.to_string b));
+          S.TCP.close flow
       );
 
     S.listen s
